44528f2330ae60dfebf46aa5b462df0d3d0ec1a0
atraydixit <atray@coralgenomics.com>
2018-12-16 13:49:48 -0800

    first commit

---
+++ octopus/src/config/config.js
@@ -0,0 +1,10 @@
+export default {
+    authProvider: "google",
+    apiKey: "AIzaSyApciQViBnGtKG5YQMpa8Xc0x3ABwESbk0",
+    authDomain: "coral-wiki.firebaseapp.com",
+    databaseURL: "https://coral-wiki.firebaseio.com",
+    projectId: "coral-wiki",
+    storageBucket: "",
+    messagingSenderId: "915632792107"
+ 
+};
